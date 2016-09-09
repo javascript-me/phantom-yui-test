a3fc5532b9d98e07aac97a62c9063ffc37c10f4f
agileabc <agileabc@sina.com>
2016-09-09 17:47:13 +0800

    Record Y console messages.

--- junitXmlParser.js
+++ junitXmlParser.js
@@ -1,4 +1,10 @@
 YUI().add('junit-xml-parser', function (Y) {
+
+    new Y.Console().on("entry", function (obj) {
+        console.log(JSON.stringify(obj.message));
+        console.log("-------------------------")
+    });
+
     if (typeof(console) !== 'undefined')
     {
         Y.Test.Runner.subscribe(Y.Test.Runner.COMPLETE_EVENT, function (obj) {
